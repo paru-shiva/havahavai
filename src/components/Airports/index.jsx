32d5595c06f68de332cc1bb73d3dae5e67fd62a5
shiva <shivamotog62@gmail.com>
2024-07-05 14:03:21 +0530

    tc

--- src/components/Airports/index.jsx
+++ src/components/Airports/index.jsx
@@ -1,12 +1,16 @@
 import "./index.css";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
+import AirportBody from "../AirportsBody";
 
 const Airports = () => {
   return (
     <div className="airportsComponent">
       <Header />
-      <Sidebar />
+      <div className="airportsMain">
+        <Sidebar />
+        <AirportBody />
+      </div>
     </div>
   );
 };
---
+++ src/components/AirportsBody/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+const AirportBody = () => {
+  return (
+    <div className="airportBodyComponent">
+      <div className="apBodyHeader">
+        <h3>Airports</h3>
+        <button className="addBtn">+ Add new</button>
+      </div>
+    </div>
+  );
+};
+
+export default AirportBody;
