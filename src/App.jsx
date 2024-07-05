2a7000167e277883f216381de1bd29c7ace676b9
shiva <shivamotog62@gmail.com>
2024-07-05 11:53:22 +0530

    fc

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Router } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="appComponent">
+      <h1>fromAppComponent</h1>
+    </div>
+  );
+}
+
+export default App;
